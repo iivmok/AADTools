f12b17f6bc519045bb211c1a6646e80dbdff8ed6
Iivari Mokelainen <iivmok@gmail.com>
2022-10-22 18:19:23 +0300

    add support for group members and group owners syncing

--- Program.cs
+++ Program.cs
@@ -7,45 +7,104 @@ var me = await graph.Me.Request().GetAsync();
 
 if (args.Length < 3)
 {
-    Console.WriteLine("dotnet run add/addAndDelete SOME_GROUP app-name");
+    Console.WriteLine("dotnet run add/sync from_group target_app_or_group [owners]");
     return;
 }
 
 var action = args[0].ToLower();
-var delete = action == "addAndDelete".ToLower();
+var delete = action == "sync".ToLower();
 var groupName = args[1];
-var appName = args[2];
+var targetName = args[2];
+var workOnGroupOwners = args.Length > 3 && args[3] == "owners"; 
 
 User[] FilterUsers(IEnumerable<DirectoryObject> list)
-    => list.Where(m => m is User).Cast<User>().ToArray();
+    => list.Where(m => m is User).Cast<User>().Where(m => !string.IsNullOrEmpty(m.Mail)).ToArray();
 
 var group = graph.Groups.Request().Filter($"DisplayName eq '{groupName}'").Expand("members").GetAsync().Result.Single();
 var members = FilterUsers(group.Members);
+Console.WriteLine("Group members:");
+foreach (var member in members)
+{
+    Console.WriteLine("  " + member.Mail);
+}
 
-var app = graph.Applications.Request().Filter($"displayName eq '{appName}'").Expand("owners").GetAsync().Result.Single();
-var owners = FilterUsers(app.Owners.Where(m => m is User));
+DirectoryObject? target = null!;
+User[] targetMembers = null!;
 
-if(delete)
-    foreach (var user in owners)
+var app = graph.Applications.Request().Filter($"displayName eq '{targetName}'").Expand("owners").GetAsync().Result.SingleOrDefault();
+var targetGroup = graph.Groups.Request().Filter($"DisplayName eq '{targetName}'").Expand(workOnGroupOwners ? "owners" : "members").GetAsync().Result.SingleOrDefault();
+if (app != null)
+{
+    Console.WriteLine($"Found app with name {targetName}");
+    target = app;
+    targetMembers = FilterUsers(app.Owners);
+}
+else if (targetGroup != null)
+{
+    Console.WriteLine($"Found group with name {targetName}");
+    target = targetGroup;
+    if (workOnGroupOwners)
     {
-        if(user.Mail == me.Mail) continue;
+        Console.WriteLine("Working on group owners, rather than members");
+        targetMembers = FilterUsers(targetGroup.Owners);
+    }
+    else
+    {
+        targetMembers = FilterUsers(targetGroup.Members);
+    }
+}
+else
+{
+    Console.WriteLine("Target is not an app or group, exiting.");
+}
+
+if (delete)
+{
+    foreach (var user in targetMembers)
+    {
+        if (user.Mail == me.Mail) continue;
 
         var shouldBeOwner = members.Any(m => m.Mail == user.Mail);
-        
+
         if (!shouldBeOwner)
         {
-            Console.WriteLine($"Deleting {user.Mail} from app owners");
-            await graph.Applications[app.Id].Owners[user.Id].Reference.Request().DeleteAsync();
+            Console.WriteLine($"Deleting {user.Mail} from target members");
+            if (app != null)
+            {
+                await graph.Applications[app.Id].Owners[user.Id].Reference.Request().DeleteAsync();
+            }
+            else if(targetGroup != null)
+            {
+                var reference = workOnGroupOwners ? graph.Groups[targetGroup.Id].Owners[user.Id] : graph.Groups[targetGroup.Id].Members[user.Id];
+                await reference.Reference.Request().DeleteAsync();
+            }
         }
     }
+}
 
 foreach (var user in members)
 {
-    var alreadyOwner = owners.Any(o => o.Mail == user.Mail);
+    var alreadyMember = targetMembers.Any(o => o.Mail == user.Mail);
     
-    if (!alreadyOwner)
+    if (!alreadyMember)
     {
-        Console.WriteLine($"Adding {user.Mail} to app owners");
-        await graph.Applications[app.Id].Owners.References.Request().AddAsync(user);
+        Console.WriteLine($"Adding {user.Mail} to target members");
+        if(app != null)
+            await graph.Applications[app.Id].Owners.References.Request().AddAsync(user);
+        else if(targetGroup != null)
+        {
+            if (workOnGroupOwners)
+            {
+                await graph.Groups[targetGroup.Id].Owners.References.Request().AddAsync(user);
+            }
+            else
+            {
+                await graph.Groups[targetGroup.Id].Members.References.Request().AddAsync(user);
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{user.Mail} already member");
     }
 }
